c1ef7c92191da7f850d4a5003ada1f67f0dee059
EngineersBox <toastcraft.info@gmail.com>
2022-05-04 01:14:16 +1000

    Created InsnUtils$insnsToString() to stringify instructions bytecode

--- instrumenter/src/main/java/com/engineersbox/continuity/instrumenter/method/MethodInstrumenter.java
+++ instrumenter/src/main/java/com/engineersbox/continuity/instrumenter/method/MethodInstrumenter.java
@@ -28,11 +28,11 @@ public class MethodInstrumenter {
         }
         methodNode.instructions.insert(InitialCutpoint.constructInitialInlineCutpoint(methodContext));
         LOGGER.trace("Method Bytecode: {}\n{}", methodNode.name, InsnUtils.insnsToString(methodNode.instructions));
-//        List<? extends ContinuationPoint> continuationPoints = methodContext.continuationPoints();
-//        for (int i = 0; i < continuationPoints.size(); i++) {
-//            final AbstractInsnNode nodeToReplace = continuationPoints.get(i).getInvokeInstruction();
-//            methodNode.instructions.insertBefore(nodeToReplace, SaveOperations.constructSaveBytecode(methodContext, i));
-//            methodNode.instructions.remove(nodeToReplace);
-//        }
+        List<? extends ContinuationPoint> continuationPoints = methodContext.continuationPoints();
+        for (int i = 0; i < continuationPoints.size(); i++) {
+            final AbstractInsnNode nodeToReplace = continuationPoints.get(i).getInvokeInstruction();
+            methodNode.instructions.insertBefore(nodeToReplace, SaveOperations.constructSaveBytecode(methodContext, i));
+            methodNode.instructions.remove(nodeToReplace);
+        }
     }
 }
