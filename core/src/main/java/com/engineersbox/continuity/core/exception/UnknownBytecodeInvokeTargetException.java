2394f17172a6094a8decad0b82caf5d003800f11
EngineersBox <toastcraft.info@gmail.com>
2022-05-09 19:48:11 +1000

    Refactored to method/constructor retrieval via @BytecodeInternal annotation using reflection

--- core/src/main/java/com/engineersbox/continuity/core/exception/UnknownBytecodeInvokeTargetException.java
+++
@@ -1,7 +0,0 @@
-package com.engineersbox.continuity.core.exception;
-
-public class UnknownBytecodeInvokeTargetException extends RuntimeException {
-    public UnknownBytecodeInvokeTargetException(final String message) {
-        super(message);
-    }
-}
